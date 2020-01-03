4d67ff5c53820ff71e8a0f2929fa9e1a6fd14c38
Rodun <yo_op@naver.com>
2020-01-04 08:38:18 +0900

    Create Line & Note Object

---
+++ RhythmGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance { get; set; }
+    public float noteSpeed;
+
+    void Awake()
+    {        
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
