be3922f0460fb20712eefc80459dab4d8b03dc06
Rodun <yo_op@naver.com>
2020-01-06 20:25:31 +0900

    Collider & Note Judge

--- RhythmGame/Assets/Scripts/GameManager.cs
+++ RhythmGame/Assets/Scripts/GameManager.cs
@@ -13,6 +13,15 @@ public class GameManager : MonoBehaviour
             instance = this;
         else if (instance != this)
             Destroy(gameObject);
+    }    
+
+    public enum judges
+    {
+        NONE =0,
+        BAD,
+        GOOD,
+        PERFECT,
+        MISS
     }
 
     void Start()
