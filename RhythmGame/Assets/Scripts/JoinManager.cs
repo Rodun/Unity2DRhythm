54f50fdda6fdae900caa5dded01834e2eed1ebb8
Rodun <yo_op@naver.com>
2020-02-15 19:32:12 +0900

    로그인 및 회원가입 구현

---
+++ RhythmGame/Assets/Scripts/JoinManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Auth;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class JoinManager : MonoBehaviour
+{
+    // Firebase인증 기능 객체
+    private FirebaseAuth auth;
+
+    // ID, Password Input UI;
+    public InputField emailInputField;
+    public InputField passwordInputField;
+
+    // Sign up result UI
+    public Text messageUI;
+
+    void Start()
+    {
+        // Firebase Auth Object Initialization
+        auth = FirebaseAuth.DefaultInstance;
+        messageUI.text = "";
+    }
+
+    bool InputCheck()
+    {
+        string email = emailInputField.text;
+        string password = passwordInputField.text;
+
+        if(email.Length < 8)
+        {
+            messageUI.text = "이메일은 8자리 이상으로 구성되어야 합니다.";
+            return false;
+        }
+        else if (password.Length < 8)
+        {
+            messageUI.text = "비밀번호는 8자리 이상으로 구성되어야 합니다.";
+            return false;
+        }
+
+        messageUI.text = "";
+        return true;
+    }
+
+    public void Check()
+    {
+        InputCheck();
+    }
+
+    public void Join()
+    {
+        if(!InputCheck())
+        {
+            return;
+        }
+        string email = emailInputField.text;
+        string password = passwordInputField.text;
+
+        // 인증객체를 이용하여 이메일과 비밀번호로 가입을 수행합니다.
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith
+        (
+            task =>
+            {
+                if(!task.IsCanceled && !task.IsFaulted)
+                {
+                    messageUI.text = "Success Sign up";
+                    SceneManager.LoadScene("LoginScene");
+                }
+                else
+                {
+                    messageUI.text = "이미 사용중이거나 형식이 올바르지 않습니다.";
+                }
+            }
+        );
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("LoginScene");
+    }
+}
---
+++ RhythmGame/Assets/Scripts/LoginManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Auth;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoginManager : MonoBehaviour
+{
+    // Firebase인증 기능 객체
+    private FirebaseAuth auth;
+
+    // ID, Password Input UI;
+    public InputField emailInputField;
+    public InputField passwordInputField;
+
+    // Sign in result UI
+    public Text messageUI;
+
+    void Start()
+    {
+        // Firebase Auth Object Initialization
+        auth = FirebaseAuth.DefaultInstance;
+        messageUI.text = "";
+    }
+
+    public void Login()
+    {
+        string email = emailInputField.text;
+        string password = passwordInputField.text;
+
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith
+        (
+            task =>
+            {
+                if(task.IsCompleted && !task.IsCanceled && !task.IsFaulted)
+                {
+                    Debug.Log("[Success Sign in] ID: " + auth.CurrentUser.UserId);
+                    PlayerInformation.auth = auth;
+                    SceneManager.LoadScene("SongSelectScene");
+                }
+                else
+                {
+                    messageUI.text = "계정을 다시 확인 해주세요.";
+                }
+            }
+        );
+    }
+
+    public void GotoJoin()
+    {
+        SceneManager.LoadScene("JoinScene"); 
+    }
+}
